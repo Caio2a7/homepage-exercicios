b3256691bc7dec2a1bfaa773a9b4b88f5f838715
Caio2a7 <caiodanielfonseca@gmail.com>
2024-02-21 09:55:06 -0300

    adicionado conteúdo nas rotas

--- exercicio-3/src/views/Contact.vue
+++ exercicio-3/src/views/Contact.vue
@@ -1,5 +1,42 @@
 <template>
     <div>
-        Contato
-    </div>
-</template>
+      <div class="bcc-main-content">
+          <main>
+              <section class="sec-main-content">
+                  <h2 style="margin-left: 46%;"><b>Contato</b></h2>
+                  <div class="content-card">
+                      <div class="content-text">
+                          <p>   
+                            <br><br>Email: petcc@dimap.ufrn.br
+
+                            <br><br>Telefone: (84)3342-2225
+
+                            <br><br>Facebook: petccufrn
+
+                            <br><br>Youtube: PET Ciência da Computação UFRN
+
+                            <br><br>Instagram: petccufrn
+
+                            <br><br>Github: PETCC-UFRN
+                          </p> 
+                      </div>
+                      <hr>
+                  </div>
+              </section>
+          </main>
+      </div>
+      <div class="bcc-footer-conteiner">
+          <footer>
+              <div class="footer-sec">
+                  <p>Telefone - (84) 3342-2299
+                      cccc@dimap.ufrn.</p>
+                  <p>Natal/RN - Brasil</p>
+              </div>
+              <div class="footer-sec">
+                  <p>© 2023, Universidade Federal do Rio Grande do Norte.</p>
+              </div>
+          </footer>
+          </div>
+      </div>
+  </template>
+  
--- exercicio-3/src/views/Forum.vue
+++ exercicio-3/src/views/Forum.vue
@@ -1,16 +1,49 @@
 <template>
     <div>
-        {{ username }}
-    </div>
-</template>
+      <div class="bcc-main-content">
+          <main>
+              <section class="sec-main-content">
+                  <h2><b>Fórum</b></h2>
+                  <div class="content-card">
+                      <div class="content-text">
+                          <p><strong style="font-weight: 600;">Qual linguagem de programação é melhor para iniciantes?</strong> 
 
-<script> 
-    export default{
-        name: 'Forum',
-        data(){
-            return {
-                username: 'Kakaroto',
-            };
-        },
-}
-</script>
+                            <br><br>Linguagens como Python e JavaScript são boas opções para iniciantes devido à sua sintaxe simples e ampla aplicação em diferentes áreas.
+
+                            <br><br><strong style="font-weight: 600;">Pergunta: Como posso me preparar para entrevistas de emprego em ciência da computação?</strong>
+
+                            <br><br>Pratique resolvendo problemas de algoritmos e estruturas de dados, revise conceitos fundamentais, como complexidade de algoritmos, e pratique respostas para perguntas comportamentais.
+
+                            <br><br><strong style="font-weight: 600;">Quais são as áreas de especialização em ciência da computação?</strong>
+
+                            <br><br>Algumas áreas incluem inteligência artificial, segurança cibernética, computação em nuvem, desenvolvimento de software, análise de dados e redes de computadores.
+
+                            <br><br><strong style="font-weight: 600;">Como posso aprender sobre desenvolvimento web?</strong>
+
+                            <br><br>Comece aprendendo HTML, CSS e JavaScript, em seguida, explore frameworks e bibliotecas populares como React, Angular ou Vue.js. Pratique construindo projetos e seguindo tutoriais online.
+
+                            <br><br><strong style="font-weight: 600;">Qual é a diferença entre Java e JavaScript?</strong>
+
+                            <br><br>Java é uma linguagem de programação usada principalmente para desenvolvimento de software de servidor e aplicativos de desktop, enquanto JavaScript é uma linguagem de programação usada para desenvolvimento web, adicionando interatividade a páginas HTML.
+                          </p> 
+                      </div>
+                      <hr>
+                  </div>
+              </section>
+          </main>
+      </div>
+      <div class="bcc-footer-conteiner">
+          <footer>
+              <div class="footer-sec">
+                  <p>Telefone - (84) 3342-2299
+                      cccc@dimap.ufrn.</p>
+                  <p>Natal/RN - Brasil</p>
+              </div>
+              <div class="footer-sec">
+                  <p>© 2023, Universidade Federal do Rio Grande do Norte.</p>
+              </div>
+          </footer>
+          </div>
+      </div>
+  </template>
+  
